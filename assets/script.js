7378a74318128db883b25071c59436ba1737e09d
Yuliia Hots <goculiya@gmail.com>
2023-12-11 10:31:48 +0100

    algo gestion fleche

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,40 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Conserve l'index de l image affichée en cours (on commence par la 1ere)
+let indexImgAffichee = 0;
+
+
+function changeImageLeft() {
+	let bannerImage = document.querySelector("#banner .banner-img");	
+	// Si nous sommes à la 1ere image 	
+	if (indexImgAffichee <= 0 ){
+		// alors on affiche la derniere
+		indexImgAffichee = slides.length - 1;
+	} else {
+		// sinon on affiche l image precedente
+		indexImgAffichee = indexImgAffichee -1;
+	}
+	bannerImage.src = "./assets/images/slideshow/" + slides[indexImgAffichee].image;	
+}
+
+function changeImageRight() {
+	let bannerImage = document.querySelector("#banner .banner-img");
+		// Si nous sommes a la dernière image
+	if (indexImgAffichee >= slides.length -1 ){
+		// Alors on affiche le 1er element
+		indexImgAffichee = 0;
+	} else {
+		// Sinon on affiche l image suivante
+		indexImgAffichee = indexImgAffichee + 1;
+	}
+	bannerImage.src = "./assets/images/slideshow/" + slides[indexImgAffichee].image;	
+}
+
+let flecheLeft = document.querySelector("#banner .arrow_left");//Récupérer un élément HTML et déclarer une variable fleche
+flecheLeft.addEventListener( "click" , changeImageLeft);
+
+let flecheRight = document.querySelector("#banner .arrow_right");//Récupérer un élément HTML et déclarer une variable fleche
+flecheRight.addEventListener( "click" , changeImageRight);
+
